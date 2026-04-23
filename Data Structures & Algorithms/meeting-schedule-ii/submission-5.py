dcda55fbf417389232fe7982fa1274cd1dd48f67
mluan23 <160914893+mluan23@users.noreply.github.com>
2026-04-22 20:31:41 -0400

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    import heapq
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # so for this, we sort by the starting times
+        # then we iterate through them
+        # if the current staritng time is larger than the 
+        # earliest ending time, we need a new room
+        # therefore add this interval to the heap as well
+        # else we'll just pop off the previous and place the new
+        # one on
+        # then we return the len of the heap
+        min_heap = []
+        heapq.heapify(min_heap)
+
+        for interval in intervals:
+            if not min_heap or interval.start < min_heap[0]:
+                heapq.heappush(min_heap, interval.end)
+            else:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap, interval.end)
+        return len(min_heap)
