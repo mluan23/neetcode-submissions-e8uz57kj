3871065515f2658ef4b94c49a6cd9fadc6001a0f
mluan23 <160914893+mluan23@users.noreply.github.com>
2026-05-13 21:12:43 -0400

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # id assume its some traversal order that can do this
+        # well we know that for each eve,
+        # there is only 1 node
+        # hmm bfs is probably better here
+        if not root:
+            return []
+        queue = deque()
+        res = []
+        queue.append(root)
+        # res.append(root.val)
+        while queue:
+            length = len(queue)
+            # add all children for the given level
+            for i in range(length):
+                cur_node = queue.popleft()
+                if i == length - 1:
+                    res.append(cur_node.val)
+                # queue.append(cur_node)
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+        return res
+
+
