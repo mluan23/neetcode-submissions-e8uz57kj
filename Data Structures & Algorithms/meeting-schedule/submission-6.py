8f4c1d9d4da097a3871d1133805e1e13bd70f950
mluan23 <160914893+mluan23@users.noreply.github.com>
2026-05-20 20:36:28 -0400

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # if the start time of one interval lies in between
+        # the start and end time of another interval:
+        # then there is a conflict and so false
+        # so the way we check this is 
+        # sort by ending times
+        intervals = sorted(intervals, key=lambda x: x.end)
+
+        prev = intervals[0]
+
+        for i in range(1,len(intervals)):
+            if intervals[i].start < prev.end:
+                return False
+            prev = intervals[i]
+        return True
+        
