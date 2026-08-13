ed9ce303b7e03ee448424c4db72b17dfc634c8bb
mluan23 <160914893+mluan23@users.noreply.github.com>
2026-08-13 16:47:30 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s.replace(" ", "")
+        s = s.lower()
+        import re
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        left =0
+        right = len(s)-1
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
