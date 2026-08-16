0e90e3fa3bd73e91853ca2c2e7ee215c63b4389e
mluan23 <160914893+mluan23@users.noreply.github.com>
2026-08-16 18:00:23 -0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 0
+        max_profit = 0 
+        # for stocks you wanna sell if your selling price is higher than your buying price
+        # if your buying price is higher than your selling price then you just gotta advance the left 
+
+        while right < len(prices):
+            buy = prices[left]
+            sell = prices[right]
+            # we got a profit, the selling price is good
+            if sell >= buy:
+                right += 1
+                max_profit = max(max_profit, sell-buy)
+            else:
+                left += 1
+                right += 1
+        return max_profit
